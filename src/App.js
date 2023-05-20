b345398c23a5bdca23619e81bbf2af547ba082d0
arthur <124684248+Arthur-mciel@users.noreply.github.com>
2023-05-19 21:46:07 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1 className='texto'><u>Fiz Esse Icone<span>!!</span></u></h1>
+
+<p className='ohmagah'>Dá Uma Olhada Lá No Meu </p>
+        <a
+          className="App-link"
+          href="https://github.com/Arthur-mciel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+
+
+export default App;
